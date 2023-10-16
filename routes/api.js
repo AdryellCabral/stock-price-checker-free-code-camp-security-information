ce8fcff32380bf488935a5a7b9f5f710e32725be
Adryell Cabral <adryellluciano@gmail.com>
2023-10-16 11:30:52 -0300

    chore: basic changes

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,27 @@
-'use strict';
+"use strict";
+const axios = require("axios");
+const { json } = require("express/lib/response");
+
+const getPrice = (stock) => {
+  axios
+    .get(
+      `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
+    )
+    .then((res) => {
+      console.log(res.data);
+      return res.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 module.exports = function (app) {
+  app.route("/api/stock-prices").get(function (req, res) {
+    let stock = req.query.stock;
+    let like = req.query.like;
+    let output = getPrice(stock);
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+    res.send(output);
+  });
 };
